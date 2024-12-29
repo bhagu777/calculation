d072c17fa140eabe0ae9f85ba9bfaa99d38fdfc6
admin <patelas652@gmail.com>
2024-12-29 17:23:34 +0530

    Commited by bhargav (29/12/2024 05:23 PM)

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  int totalPoints = 0;
+  RxInt totalPoints = 0.obs;
   int totalLives = 3;
   String question = '';
   int answerCorrect = 0;
@@ -34,11 +34,27 @@ class HomeController extends GetxController {
     });
   }
 
+  // get Container Height Function//
+  double getContainerHeight() {
+    return 150 + (10 - timeLeft) * 12;
+  }
+
+  // get Color Based On Time Function //
+  Color getColorBasedOnTime() {
+
+    double red = 255;
+    double green = 255 - ((255 - 105) * (10 - timeLeft) / 10);
+    double blue = 255 - ((255 - 180) * (10 - timeLeft) / 10);
+
+    return Color.fromRGBO(red.toInt(), green.toInt(), blue.toInt(), 1.0);
+  }
+
   // reset Timer Function //
   void resetTimer() {
     timer?.cancel();
     timeLeft = 10;
     startTimer();
+    displayValue.value = '';
     update();
   }
 
@@ -47,18 +63,18 @@ class HomeController extends GetxController {
     Random random = Random();
     int num1 = random.nextInt(10) + 1;
     int num2 = random.nextInt(10) + 1;
-    List<String> operators = ['+', '-', '*'];
+    List<String> operators = ['+', '-', 'X'];
     String operator = operators[random.nextInt(3)];
 
     switch (operator) {
       case '+':
-        answerCorrect = num1 + num2;
+        answerCorrect = num2 + num1;
         break;
       case '-':
-        answerCorrect = num1 - num2;
+        answerCorrect = num2 - num1;
         break;
       case '*':
-        answerCorrect = num1 * num2;
+        answerCorrect = num2 * num1;
         break;
     }
 
@@ -70,7 +86,7 @@ class HomeController extends GetxController {
   // check Answer Right Or Wrong Function //
   void checkAnswerRightOrWrong() {
     if (int.tryParse(rightAnswer) == answerCorrect) {
-      totalPoints++;
+      totalPoints.value++;
       correctAmount.clear();
       generateNewQuestion();
       resetTimer();
@@ -117,9 +133,10 @@ class HomeController extends GetxController {
   void showGameOverDialog() {
     showDialog(
       context: Get.context!,
+      barrierDismissible: false,
       builder: (_) => AlertDialog(
         title: const Center(child: Text('Game Over')),
-        content: Text('Your Score: $totalPoints'),
+        content: Text('Your Score: ${totalPoints.value}'),
         actions: [
           Row(
             children: [
@@ -168,7 +185,7 @@ class HomeController extends GetxController {
 
   // restart Game Function//
   void restartGame() {
-    totalPoints = 0;
+    totalPoints.value = 0;
     totalLives = 3;
     update();
     generateNewQuestion();
@@ -179,5 +196,102 @@ class HomeController extends GetxController {
   void dispose() {
     timer?.cancel();
     super.dispose();
+  }
+
+  RxString displayValue = ''.obs;
+
+  // Function to handle key press
+  void onKeyPressed(String value) {
+    displayValue.value +=
+        value;
+    update();
+  }
+
+  // text clear function //
+  void clear() {
+    displayValue.value = '';
+    update();
+  }
+
+  // back space Function //
+  void backspace() {
+    if (displayValue.value.isNotEmpty) {
+      displayValue.value =
+          displayValue.value.substring(0, displayValue.value.length - 1);
+    }
+    update();
+  }
+
+  // on Clear Function //
+  void onClear() {
+    displayValue.value = "";
+    update();
+  }
+
+  // on Submit button //
+  void onSubmit() {
+    print("saddadsd");
+    if (int.tryParse(displayValue.value) == answerCorrect) {
+      _handleCorrectAnswer();
+    } else {
+      handleWrongAnswer1();
+    }
+  }
+
+// Handle correct answer submission //
+  void _handleCorrectAnswer() {
+
+    print("Submitted Answer: $displayValue");
+    totalPoints.value++;
+
+    generateNewQuestion();
+    resetTimer();
+    update();
+
+    displayValue.value = "";
+    update();
+  }
+
+// Handle wrong answer submission //
+  void handleWrongAnswer1() {
+    if (totalLives > 0) {
+      totalLives--;
+      correctAmount.clear();
+      _showWrongAnswerSnackBar();
+      update();
+      if (totalLives == 0) {
+        _handleGameOver();
+      } else {
+        _prepareForNextAttempt();
+      }
+    }
+  }
+
+// Show snackbar for wrong answer feedback //
+  void _showWrongAnswerSnackBar() {
+    if (!isSnackBarShown) {
+      ScaffoldMessenger.of(Get.context!).showSnackBar(
+        const SnackBar(
+          content: Text('Answer is incorrect'),
+          duration: Duration(seconds: 2),
+          backgroundColor: Colors.red,
+        ),
+      );
+      isSnackBarShown =
+          true;
+    }
+  }
+
+// Handle game over logic  //
+  void _handleGameOver() {
+    timer?.cancel();
+    showGameOverDialog();
+  }
+
+// Prepare for the next question and reset the timer //
+  void _prepareForNextAttempt() {
+    isSnackBarShown = false;
+    generateNewQuestion();
+    resetTimer();
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -15,103 +15,186 @@ class HomeView extends GetView<HomeController> {
         builder: (context) {
           return Scaffold(
             backgroundColor: Colors.white,
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  height: 100,
-                  color: Colors.purple,
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 30),
-                      Expanded(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: List.generate(
-                                3,
-                                (index) => Icon(
-                                  index < controller.totalLives
-                                      ? Icons.favorite
-                                      : Icons.favorite_border,
+            body: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    height: 100,
+                    color: Colors.purple,
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 30),
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: List.generate(
+                                  3,
+                                  (index) => Icon(
+                                    index < controller.totalLives
+                                        ? Icons.favorite
+                                        : Icons.favorite_border,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                'Points: ${controller.totalPoints.value}',
+                                style: const TextStyle(
+                                  fontSize: 18,
                                   color: Colors.white,
                                 ),
                               ),
-                            ),
-                            Text(
-                              'Points: ${controller.totalPoints}',
-                              style: const TextStyle(
-                                fontSize: 18,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    height: controller.getContainerHeight(),
+                    color: controller.getColorBasedOnTime(),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    controller.question,
+                    style: TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.number,
+                      readOnly: true,
+                      controller: TextEditingController(
+                          text: controller.displayValue.value),
+                      textInputAction: TextInputAction.done,
+                      decoration: InputDecoration(
+                        hintText: 'Enter Your Answer',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
                       ),
-                    ],
+                      onChanged: (value) {
+                        controller.rightAnswer = value;
+                      },
+                      onSubmitted: (value) {
+                        controller.rightAnswer = value;
+                        controller.checkAnswerRightOrWrong();
+                      },
+                    ),
                   ),
-                ),
-                Container(
-                  height: 150,
-                  color:
-                      Colors.purpleAccent.withOpacity(controller.timeLeft / 10),
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  controller.question,
-                  style: TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey.shade500,
+                  const SizedBox(
+                    height: 20,
                   ),
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  'Time Left: ${controller.timeLeft}',
-                  style: const TextStyle(
-                      fontSize: 18,
-                      color: Colors.red,
-                      fontWeight: FontWeight.w600),
-                ),
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: TextField(
-                    textAlign: TextAlign.center,
-                    keyboardType: TextInputType.number,
-                    controller: controller.correctAmount,
-                    textInputAction: TextInputAction.done,
-                    decoration: InputDecoration(
-                      hintText: 'Enter Your Answer',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter ,
+                    child: Container(
+                      color: Colors.purple.withOpacity(0.5),
+                      padding: EdgeInsets.all(16),
+                      child: GridView.builder(
+                        shrinkWrap: true,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 20,
+                          mainAxisExtent: 70,
+                          mainAxisSpacing: 20,
+                        ),
+                        padding: EdgeInsets.zero,
+                        itemCount: 12,
+                        itemBuilder: (context, index) {
+                          if (index == 9) {
+                            return SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: ElevatedButton(
+                                onPressed: () => controller.onClear(),
+                                style: ElevatedButton.styleFrom(
+                                  padding: EdgeInsets.all(20),
+                                  backgroundColor: Colors.redAccent,
+                                ),
+                                child: const Text(
+                                  "CE",
+                                  style: TextStyle(
+                                      fontSize: 18, color: Colors.white),
+                                ),
+                              ),
+                            );
+                          } else if (index == 10) {
+                            // "0" button
+                            return SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: ElevatedButton(
+                                onPressed: () => controller.onKeyPressed("0"),
+                                style: ElevatedButton.styleFrom(
+                                  padding: EdgeInsets.all(20),
+                                  backgroundColor: Colors.purple,
+                                ),
+                                child: const Text(
+                                  "0",
+                                  style: TextStyle(
+                                      fontSize: 24, color: Colors.white),
+                                ),
+                              ),
+                            );
+                          } else if (index == 11) {
+                            // Submit button
+                            return SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  controller.onSubmit();
+                                  controller.update();
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  padding: EdgeInsets.all(20),
+                                  backgroundColor: Colors.green,
+                                ),
+                                child: const Icon(Icons.check,
+                                    size: 24, color: Colors.white),
+                              ),
+                            );
+                          } else {
+                            // Numbers 1-9
+                            return SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: ElevatedButton(
+                                onPressed: () => controller
+                                    .onKeyPressed((index + 1).toString()),
+                                style: ElevatedButton.styleFrom(
+                                  padding: EdgeInsets.all(20),
+                                  backgroundColor: Colors.purple.withOpacity(0.5),
+                                ),
+                                child: Text(
+                                  (index + 1).toString(),
+                                  style: TextStyle(
+                                      fontSize: 24, color: Colors.white),
+                                ),
+                              ),
+                            );
+                          }
+                        },
                       ),
                     ),
-                    onChanged: (value) {
-                      controller.rightAnswer = value;
-                    },
-                    onSubmitted: (value) {
-                      controller.rightAnswer = value;
-                      controller.checkAnswerRightOrWrong();
-                    },
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  onPressed: controller.checkAnswerRightOrWrong,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.purple,
-                  ),
-                  child: const Text(
-                    'Submit',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         });
